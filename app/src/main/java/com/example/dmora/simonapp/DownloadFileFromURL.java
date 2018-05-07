579cb9fd33bc0c7f842b9a99e10d42d4f888304c
Daniel Morales Gonzalez <daniel.morales.gonzalez@estudiant.upc.edu>
2018-05-07 13:17:13 +0200

    proyecto enviado

---
+++ app/src/main/java/com/example/dmora/simonapp/DownloadFileFromURL.java
@@ -0,0 +1,8 @@
+package com.example.dmora.simonapp;
+
+/**
+ * Created by DanielMoralesGonzale on 12/03/2018.
+ */
+
+public class DownloadFileFromURL {
+}
---
+++ app/src/main/java/com/example/dmora/simonapp/Tutorial.java
@@ -0,0 +1,8 @@
+package com.example.dmora.simonapp;
+
+/**
+ * Created by DanielMoralesGonzale on 11/03/2018.
+ */
+
+public class Tutorial {
+}
